49ea03efb56cbbcd1382292135fe9641afc9d38a
Olexiy Zamkoviy <olexiy.z@gmail.com>
2013-08-21 17:34:56 +0300

    Added bootstrap-timepicker 0.2.3

---
+++ bootstrap-timepicker/0.2.3/get.sh
@@ -0,0 +1,2 @@
+git clone https://github.com/jdewit/bootstrap-timepicker.git
+cd bootstrap-timepicker && git reset --hard 796688ba405916186aeae4326165b219f4c6659d
---
+++ bootstrap-timepicker/0.2.3/serve.lisp
@@ -0,0 +1,2 @@
+(serve-file "bootstrap-timepicker/compiled/timepicker.css" "/pub/stylesheets/bootstrap-timepicker.css")
+(serve-file "bootstrap-timepicker/js/bootstrap-timepicker.js" "/pub/scripts/bootstrap-timepicker.js")
