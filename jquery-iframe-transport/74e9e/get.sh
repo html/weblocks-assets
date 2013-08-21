9933b7dc4ce52d165c82cee9ac87e164f1673edd
Olexiy Zamkoviy <olexiy.z@gmail.com>
2013-08-21 23:43:08 +0300

    Added jquery-iframe-transport 74e9e

---
+++ jquery-iframe-transport/74e9e/get.sh
@@ -0,0 +1,1 @@
+wget https://raw.github.com/html/jquery-iframe-transport/74e9eb794b60281f6772f1e0b79aeba006d98c6f/jquery.iframe-transport.js
---
+++ jquery-iframe-transport/74e9e/serve.lisp
@@ -0,0 +1,1 @@
+(serve-file "jquery.iframe-transport.js" "/pub/scripts/jquery.iframe-transport.js")
