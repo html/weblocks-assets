33c953a42a5ebdd88aaaff322d91c1e9c969a50b
Olexiy Zamkoviy <olexiy.z@gmail.com>
2013-08-21 23:08:03 +0300

    Added bootstrap-datepicker 1.2.0

---
+++ bootstrap-datepicker/1.2.0/get.sh
@@ -0,0 +1,3 @@
+wget https://github.com/eternicode/bootstrap-datepicker/archive/1.2.0.tar.gz
+tar zxvf 1.2.0.tar.gz
+rm 1.2.0.tar.gz
---
+++ bootstrap-datepicker/1.2.0/serve.lisp
@@ -0,0 +1,3 @@
+(serve-file "bootstrap-datepicker-1.2.0/css/datepicker.css" "/pub/stylesheets/bootstrap-datepicker.css")
+(serve-file "bootstrap-datepicker-1.2.0/js/bootstrap-datepicker.js" "/pub/scripts/bootstrap-datepicker.js")
+(serve-directory "bootstrap-datepicker-1.2.0/js/locales" "/pub/scripts/bootstrap-datepicker-locales")
