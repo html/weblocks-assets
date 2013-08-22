6a602dbd6b9ab167bb65c4db168aacf44b1953f5
Olexiy Zamkoviy <olexiy.z@gmail.com>
2013-08-22 15:16:06 +0300

    Added tinymce 4.0.4

---
+++ tinymce/4.0.4/get.sh
@@ -0,0 +1,3 @@
+wget "http://download.moxiecode.com/tinymce/tinymce_4.0.4_dev.zip"
+unzip -x tinymce_4.0.4_dev.zip
+rm tinymce_4.0.4_dev.zip
---
+++ tinymce/4.0.4/serve.lisp
@@ -0,0 +1,1 @@
+(serve-directory "tinymce/js/tinymce" "tinymce")
