ddf0cc71dc1bc3752ab0fb991aae19731f5cc723
Olexiy Zamkoviy <olexiy.z@gmail.com>
2014-02-11 11:36:47 +0200

    Added codemirror-mustache-addon rev-2

---
+++ codemirror-mustache-addon/rev-2/get.sh
@@ -0,0 +1,2 @@
+wget https://gist.github.com/html/8015604/raw/c27985ab121a3fc248e8b85f4fa247e556872fba/codemirror-mustache.js
+echo ".cm-mustache {color: #0ca;}" > mustache-mode.css
---
+++ codemirror-mustache-addon/rev-2/serve.lisp
@@ -0,0 +1,2 @@
+(serve-file "codemirror-mustache.js" "/pub/scripts/codemirror-mustache.js")
+(serve-file "mustache-mode.css" "/pub/stylesheets/mustache-mode.css")
