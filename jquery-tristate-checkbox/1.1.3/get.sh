d490b59e215d3ba943479ce72be1223eec2f3a59
Olexiy Zamkoviy <olexiy.z@gmail.com>
2014-04-24 00:41:12 +0300

    Added jquery-tristate-checkbox 1.1.3

---
+++ jquery-tristate-checkbox/1.1.3/get.sh
@@ -0,0 +1,2 @@
+wget https://github.com/jlbruno/jQuery-Tristate-Checkbox-plugin/archive/1.1.3.tar.gz
+tar zxvf 1.1.3.tar.gz
---
+++ jquery-tristate-checkbox/1.1.3/serve.lisp
@@ -0,0 +1,2 @@
+(serve-file "jQuery-Tristate-Checkbox-plugin-1.1.3/jquery.tristate.js" "/pub/scripts/jquery.tristate.js")
+(serve-file "jQuery-Tristate-Checkbox-plugin-1.1.3/jquery.tristate.min.js" "/pub/scripts/jquery.tristate.min.js")
